de6dfd2aeb95d1312c1928173d6638ef126814db
Beatriz Manrique <amanrique068@gmail.com>
2022-11-04 19:53:28 -0500

    Configuracion HTML y JSP: enemigos Y actualizacion BD

--- LAB8/src/main/java/com/example/lab8/Beans/Enemigo.java
+++ LAB8/src/main/java/com/example/lab8/Beans/Enemigo.java
@@ -1,6 +1,58 @@
 package com.example.lab8.Beans;
 
 public class Enemigo {
-    String nombre;
-    
+    private String nombre;
+    private String clase;
+    private int ataque;
+    private int experiencia;
+    private String objetoDado;
+    private String genero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public String getObjetoDado() {
+        return objetoDado;
+    }
+
+    public void setObjetoDado(String objetoDado) {
+        this.objetoDado = objetoDado;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
